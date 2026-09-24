9bd1821b9c098e96267d72f04700006ceafc261c
kwan0907 <63507197+kwan0907@users.noreply.github.com>
2026-09-24 16:26:49 +0800

    Cache new app icon

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE="crab-pos-shell-v1";
-const ASSETS=["/","/styles.css","/app.js","/config.js","/manifest.webmanifest","/icons/icon.svg"];
+const CACHE="crab-pos-shell-v2";
+const ASSETS=["/","/styles.css","/app.js","/config.js","/manifest.webmanifest","/icons/app-icon.png","/icons/icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
